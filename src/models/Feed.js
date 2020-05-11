92d4935c62af897090080249bec2fe17b6d59768
Chingiz <chingiz.bakhishov@gmail.com>
2020-05-10 18:02:07 -0600

    Add samehered flag

--- src/models/Feed.js
+++ src/models/Feed.js
@@ -14,7 +14,7 @@ async function getUserFeed(firstPersonId, userId, count = 20, lastDate) {
             'topic', json_build_object('id', topics.id, 'name', INITCAP(topics.name)),
             'location', cities.name || ', ' || countries.name,
             'sameHere', COALESCE(sh.count, 0),
-            'sameHered', sh.same_hered,
+            'sameHered', COALESCE(sh.same_hered, FALSE),
             'postedBy', users.obj
         ) ORDER BY posts.created DESC) AS posts
     FROM posts
@@ -25,7 +25,7 @@ async function getUserFeed(firstPersonId, userId, count = 20, lastDate) {
     INNER JOIN cities ON city_id = cities.id
     INNER JOIN states ON states.id = cities.state_id
     INNER JOIN countries ON countries.id = states.country_id
-    LEFT JOIN (SELECT post_id, count(user_id), ${firstPersonId}=ANY(array_agg(user_id)) AS same_hered FROM same_heres GROUP BY post_id) AS sh ON sh.post_id = ap.id
+    LEFT JOIN (SELECT post_id, count(user_id), ${firstPersonId}=ANY(array_agg(user_id)) AS same_hered FROM same_heres GROUP BY post_id) AS sh ON sh.post_id = ap.post_id
     INNER JOIN 
     (SELECT
         users.id,
