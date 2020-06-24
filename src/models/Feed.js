eaeaec23c63d5e14259750838ec9be82716214f7
Elnar  Sharifli <esharifli@homit.ca>
2020-06-24 00:37:06 -0600

    get posts BE call modified to implement git subtopics posts

--- src/models/Feed.js
+++ src/models/Feed.js
@@ -1,4 +1,4 @@
-async function getUserFeed(firstPersonId, { userId, topicId, postId, lastDate, count = 20}) {
+async function getUserFeed(firstPersonId, { userId, topicId, subTopicId, postId, lastDate, count = 20}) {
     let whereStr = '';
     let whereArr = [];
     let counter = 1;
@@ -18,6 +18,12 @@ async function getUserFeed(firstPersonId, { userId, topicId, postId, lastDate, c
 
     if (topicId) {
         whereArr.push(`topics.id=$${counter}`);
+        params.push(topicId);
+        counter++;
+    }
+
+    if (subTopicId) {
+        whereArr.push(`subtopics.id=$${counter}`);
         params.push(topicId);
         counter++;
     }
