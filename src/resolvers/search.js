103fc9cd0dad0b991dabe859fdf3fbbb25bf9a77
Elnar  Sharifli <esharifli@homit.ca>
2020-09-30 21:48:28 -0600

    search.js updated to work without user signed in

--- src/resolvers/search.js
+++ src/resolvers/search.js
@@ -4,31 +4,33 @@ const Search = require('../models/Search');
 async function search(parent, { text }, { req }) {
     let result = {};
 
-    if (!Auth.isUserAuthorised(req)) throw new Auth.AuthenticationError();
-
     let userId = req.session.user && req.session.user.id;
     let totCount = 0;
+    let result_TopicPosts = [];
+    let result_locationPosts = [];
 
     let result_Users = await Search.searchUsers(text, 7);
-    
+
     if (!result_Users) throw new Error('Error while geeting search results for users');
-    
+
     totCount = result_Users.length;
-    
-    let result_TopicPosts = await Search.searchPosts(userId, text, null, (7 + 5) - totCount);
 
-    if (!result_TopicPosts) throw new Error('Error while geeting search results for topic');
+    if (userId) {
+        result_TopicPosts = await Search.searchPosts(userId, text, null, (7 + 5) - totCount);
 
-    totCount = totCount + result_TopicPosts.length;
+        if (!result_TopicPosts) throw new Error('Error while geeting search results for topic');
 
-    let result_locationPosts = await Search.searchPosts(userId, null, text, 15 - totCount);
-    
-    if (!result_locationPosts) throw new Error('Error while geeting search results for country');
+        totCount = totCount + result_TopicPosts.length;
+
+        result_locationPosts = await Search.searchPosts(userId, null, text, 15 - totCount);
+
+        if (!result_locationPosts) throw new Error('Error while geeting search results for country');
+    }
 
     return result = {
         users: result_Users,
         topicPosts: result_TopicPosts,
-        locationPosts: result_locationPosts
+        locationPosts: result_locationPosts 
     };
 }
 
